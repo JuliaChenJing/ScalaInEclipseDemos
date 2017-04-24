2b05e4dd19b26c4a2bf40dcda3aa67befafcfcdd
Julia Chen Jing <juliachenjing@gmail.com>
2017-04-24 18:06:32 -0500

    Add files via upload

---
+++ src/WordCount.scala
@@ -0,0 +1,31 @@
+import java.io.File
+import java.io._
+
+import scala.collection.immutable.ListMap;
+
+object WordCount {
+  def main(args: Array[String]) {
+    val output = scala.io.Source.fromFile("./wordCountInput.txt","ISO-8859-1")
+
+      .getLines
+      .flatMap(_.split("\\W+"))
+      .filter(x => x.matches("[A-Za-z]+") && x.length>1)
+      .foldLeft(Map.empty[String, Int]){
+
+        (count, word) => count + (word -> (count.getOrElse(word, 0) + 1))
+
+      }
+
+    val sortedOutput = ListMap(output.toSeq.sortWith(_._1.toLowerCase < _._1.toLowerCase):_*)
+
+    val writer = new PrintWriter(new File("./wordCountOutput.txt" ))
+
+    for(a <- sortedOutput) {
+      writer.write(a.toString() + "\n")
+    }
+    writer.close()
+
+  }
+
+
+}
